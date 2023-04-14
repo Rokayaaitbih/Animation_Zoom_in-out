b245818a0fcdc5359e9c469363fc8d0546500c8a
Aitbih Rokaya <rokayaaitbih@gmail.com>
2023-04-14 21:35:40 +0000

    Animation ZOOM IN-OUT

---
+++ app/src/main/java/com/example/animation_zoom_in_out/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.animation_zoom_in_out;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Animation anim;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text=(TextView)findViewById(R.id.text);
+        anim= AnimationUtils.loadAnimation(this,R.anim.scale);
+
+
+        text.setAnimation(anim);
+    }
+}
